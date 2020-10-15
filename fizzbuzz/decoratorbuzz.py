901df504f8c1f92971d4eb3923fdc788880059fa
Magnus Dreyer <magnus.dreyer@gmail.com>
2020-10-15 12:51:49 +0200

    Updated decoratorbuzz to accept a lamda as input to the decorator

--- fizzbuzz/decoratorbuzz.py
+++ fizzbuzz/decoratorbuzz.py
@@ -7,9 +7,11 @@ def something(funktion):
         def wrapper(*args, **kwargs):
             first_arg = args[0]
             second_arg = args[1]
-
             output = second_arg
-            output += str(funktion(first_arg))
+            if isinstance(output, int):
+                output = funktion(first_arg)
+            else:
+                output += str(funktion(first_arg))
 
             if not output:
                 output = first_arg
@@ -17,10 +19,12 @@ def something(funktion):
         return wrapper
     return fizzbuzz
 
+
 @something(lambda a  : "Fizz" if a % 3 == 0  else "")
 @something(lambda a  : "Buzz" if a % 5 == 0  else "")
-def my_print(just_a_number, i):
-    print(i)
+@something(lambda a  : "Bonanza" if a % 7 == 0  else "")
+def my_print(just_a_number, output):
+    print(output)
 
 
 def decoratorbuzz():
