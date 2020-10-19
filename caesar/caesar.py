b7d8695b43f58754005b9613c6f5a4cb2ebad2cb
Magnus Dreyer <magnus.dreyer@gmail.com>
2020-10-19 23:12:11 +0200

    Rewrite for cleaner code

--- caesar/caesar.py
+++ caesar/caesar.py
@@ -2,20 +2,20 @@ import string
 
 
 def caesar_code(input_string, key):
+    key = key % 26 
     def encode(char, key):
         if char in string.ascii_letters:
             byte  = ord(char) + key
+            # Not a ascii char anymore. Lets switch it down
             if chr(byte) not in string.ascii_letters:
                 byte -= 26
             return(chr(byte))
         return char
 
-    result = ''
-    for char in input_string:
-        result += encode(char, key)
-
+    result = ''.join([encode(char, key) for char in input_string])
     return result
 
 
 if __name__ == "__main__":
     print(caesar_code('Lille kat, lille kat, lille kat på vejen.', 12))
+
