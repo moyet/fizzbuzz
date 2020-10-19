97efc965bc1d0baf27ac39da7df6312847e0579a
Magnus Dreyer <magnus.dreyer@gmail.com>
2020-10-19 20:24:39 +0200

    Ceasar code added

---
+++ caesar/caesar.py
@@ -0,0 +1,21 @@
+import string
+
+
+def caesar_code(input_string, key):
+    def encode(char, key):
+        if char in string.ascii_letters:
+            byte  = ord(char) + key
+            if chr(byte) not in string.ascii_letters:
+                byte -= 27
+            return(chr(byte))
+        return char
+
+    result = ''
+    for char in input_string:
+        result += encode(char, key)
+
+    return result
+
+
+if __name__ == "__main__":
+    print(caesar_code('Lille kat, lille kat, lille kat på vejen.', 12))
